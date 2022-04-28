61d1d5e15dedd70492ab1e3ed54658c8032116af
Abhilash Hazarika <abhilash@satvig.com>
2022-04-27 21:55:53 -0400

    Banner component

--- medium/pages/index.tsx
+++ medium/pages/index.tsx
@@ -1,17 +1,22 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Banner from '../components/Banner'
 import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className='max-w-7xl mx-auto'>
       <Head>
         <title>Medium 2.0 Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header />
+      <Banner />
+
+      {/* posts */}
+      
     </div>
   )
 }
