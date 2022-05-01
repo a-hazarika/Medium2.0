58d26af55b16e5e6e96f6568f2c99e27348f687b
Abhilash Hazarika <abhilash@satvig.com>
2022-04-30 23:37:09 -0400

    Showing no comments message when there are no comments for a post

--- medium/components/Comments.tsx
+++ medium/components/Comments.tsx
@@ -12,8 +12,10 @@ const Comments = ({ comments }: PostComments) => {
         <div className='flex flex-col p-10 my-10 max-w-lg sm:max-w-2xl mx-auto shadow shadow-yellow-500'>
             <h3 className='text-4xl'>Comments</h3>
             <hr className='pb-2' />
-
-            {comments.map((comment) => (
+            {comments.length === 0 && <>
+                <h1 className='text-xl'>There are no comments!</h1>
+            </>}
+            {comments.length > 0 && comments.map((comment) => (
                 <div key={comment._id}>
                     <Comment comment={comment} />
                 </div>
